f678eb2f967cc342aa0ab35e1c49d7202f13eb0f
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2022-03-01 23:05:10 +0600

    Added mongo schema, crud routes, error handling

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -8,10 +8,23 @@ const Todo = new mongoose.model("Todo", todoSchema);
 
 //GET ALL THE TODOS
 router.get('/', async (req, res) => {
+  await Todo.find({ status: 'active' }, (err, data) => {
+    if (err) {
+      res.status(500).json({
+        error: 'There was a server side error',
+      });
+    } else {
+      res.status(200).json({
+        result: data,
+        message: 'Success!',
+      });
+    }
+  })
 
 })
 //GET one THE TODO by id
 router.get('/:id', async (req, res) => {
+
 
 })
 //POST A TODO
@@ -42,14 +55,17 @@ router.post('/all', async (req, res) => {
       });
     }
   })
-
 })
+
 //UPDATE TODO
 router.put('/:id', async (req, res) => {
-  await Todo.updateOne({ _id: req.params.id }, {
+  const result = await Todo.findByIdAndUpdate({ _id: req.params.id }, {
     $set: {
       status: 'active'
-    }
+    },
+  }, {
+    new: true,
+    useFindAndModify: false,
   }, (err) => {
     if (err) {
       res.status(500).json({
@@ -61,10 +77,42 @@ router.put('/:id', async (req, res) => {
       });
     }
   })
+  console.log(result);
 })
+
 //DELETE TODO
 router.delete('/:id', async (req, res) => {
-
+  await Todo.deleteOne({ _id: req.params.id }, (err) => {
+    if (err) {
+      res.status(500).json({
+        error: 'There was a server side error',
+      });
+    } else {
+      res.status(200).json({
+        message: 'Deleted Successfully!',
+      });
+    }
+  })
 })
 
 module.exports = router;
+
+
+let input = { student: 5, teacher: 4, stuff: 3, '6': 6, '9': 8, '7': 1 };
+
+let keys = [];
+let values = [];
+//first iterate over key,values of the input object
+Object.entries(input).forEach(([key, value]) => {
+  //check if key and value are diffrent, put them in the related array
+  if (key != value) {
+    keys.push(key);
+    values.push(value);
+  }
+  else {
+    return;
+  }
+});
+
+console.log('keys:', keys);
+console.log('values:', values);
